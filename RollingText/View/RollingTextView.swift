b99609befa2cad6f78146bbed45a6a5aef0a3d08
hyun <hangam31@gmail.com>
2022-12-04 23:51:05 +0900

    Test commit

---
+++ RollingText/View/RollingTextView.swift
@@ -0,0 +1,22 @@
+//
+//  RollingTextView.swift
+//  RollingText
+//
+//  Created by 강현 on 2022/12/04.
+//
+
+import SwiftUI
+
+struct RollingTextView: View {
+    var font: Font = .largeTitle
+    var weight: Font.Weight = .regular
+    var body: some View {
+        Text("helo")
+    }
+}
+
+struct RollingTextView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
