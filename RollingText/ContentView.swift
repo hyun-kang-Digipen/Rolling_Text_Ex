3874fcf199b7ed28549ac9e1bc859bd62fefb564
hyun <hangam31@gmail.com>
2022-12-05 00:20:19 +0900

    update Rolling Text - finish practice

--- RollingText/ContentView.swift
+++ RollingText/ContentView.swift
@@ -8,10 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var value: Int = 111
     var body: some View {
         NavigationView{
             VStack{
+                RollingTextView(font: .system(size: 55), weight: .black, value: $value)
                 
+                Button("Change Value"){
+                    value = .random(in: 200...300)
+                }
             }
             .padding()
             .navigationTitle("Rolling Counter")
--- RollingText/View/RollingTextView.swift
+++ RollingText/View/RollingTextView.swift
@@ -10,10 +10,73 @@ import SwiftUI
 struct RollingTextView: View {
     var font: Font = .largeTitle
     var weight: Font.Weight = .regular
+    
+    @Binding var value: Int
+    @State var animationRange: [Int] = []
+    
     var body: some View {
-        Text("helo")
+        HStack(spacing: 0) {
+            ForEach(0..<animationRange.count, id: \.self) { index in
+                Text("8")
+                    .font(font)
+                    .fontWeight(weight)
+                    .opacity(0)
+                    .overlay {
+                        GeometryReader{ proxy in
+                            let size = proxy.size
+                            
+                            VStack(spacing: 0) {
+                                ForEach(0...9, id: \.self){ number in
+                                    Text("\(number)")
+                                        .font(font)
+                                        .fontWeight(weight)
+                                        .frame(width: size.width, height: size.height, alignment: .center)
+                                }
+                            }
+                            .offset(y: -CGFloat(animationRange[index]) * size.height)
+                        }
+                        .clipped()
+                    }
+            }
+        }
+        .onAppear {
+            animationRange = Array(repeating: 0, count: "\(value)".count)
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.06){
+                updateText()
+            }
+        }
+        .onChange(of: value){ newValue in
+            updateText()
+        }
     }
+    
+    func updateText(){
+        let stringValue = "\(value)"
+        for (index, value) in zip(0..<animationRange.count, stringValue){
+            
+            var fraction = Double(index) * 0.15
+            
+            fraction = (fraction > 0.5 ? 0.5 : fraction)
+            
+            withAnimation(.interactiveSpring(response: 0.8, dampingFraction: 1 + fraction, blendDuration: 1 + fraction)){
+                animationRange[index] = (String(value) as NSString).integerValue
+            }
+        }
+    }
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct RollingTextView_Previews: PreviewProvider {
     static var previews: some View {
