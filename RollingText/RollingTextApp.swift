a52f936b172b7520466dc301d8bc8cd574f2e466
hyun <hangam31@gmail.com>
2022-12-04 23:39:07 +0900

    Initial Commit

---
+++ RollingText/RollingTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollingTextApp.swift
+//  RollingText
+//
+//  Created by 강현 on 2022/12/04.
+//
+
+import SwiftUI
+
+@main
+struct RollingTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
